6356d50ad6b527ab151450888ffbbb98731156f1
zGabzinnn <gabrielandrecogo@outlook.com>
2026-04-29 12:17:30 -0300

    Término da validação do titulo
    Adição feita por: Gabriel André

--- ValidaçãoTitulo.py
+++ ValidaçãoTitulo.py
@@ -4,7 +4,8 @@ def validar_titulo(titulo):
     numeros = list(map(int, titulo))
     soma = 0
     peso = 2
-
+    
+    # Primeiro dígito
     for i in range(7, -1, -1):
         soma += numeros[i] * peso
         peso += 1
@@ -16,3 +17,21 @@ def validar_titulo(titulo):
         digito1 = 0
     else:
         digito1 = 11 - resto
+
+    # Segundo dígito
+    soma = 0
+    peso = 7
+
+    for i in range(8, 10):
+        soma += numeros[i] * peso
+        peso -= 1
+
+    soma += digito1 * 9
+
+    resto = soma % 11
+    if resto < 2:
+        digito2 = 0
+    else:
+        digito2 = 11 - resto
+
+    return digito1 == numeros[10] and digito2 == numeros[11]
