d647993b9e1ce5a8c2a87df80afa31c5894846a1
YYenoo <bernardo.gb@puccampinas.edu.br>
2026-05-05 20:23:16 -0300

    While
    coloquei um while true para o comando ficar rodando ate a pessoa que esta controlando desejar sair

--- Hub.py
+++ Hub.py
@@ -1,15 +1,22 @@
 from Management import iniciar_management
 from busca_eleitores import iniciar_votacao
 def iniciar_hub():
-    print('='*15,'H U B','='*15)
-    print(f'{"Bem-vindo ao Hub de Gerenciamento e Votação!":^35}')
-    print(f'{"Selecione uma opção para continuar:":^35}')
-    print("\t1. Gerenciamento")
-    print("\t2. Votação")
-    escolha = input('Digite o número da opção desejada: ')
-    match escolha:
-        case '1': iniciar_management()
-        case '2': iniciar_votacao()
-        case _: print('Opção inválida. Por favor, tente novamente.')
-
+    while True:
+        print('='*15,'H U B','='*15)
+        print(f'{"Bem-vindo ao Hub de Gerenciamento e Votação!":^35}')
+        print(f'{"Selecione uma opção para continuar:":^35}')
+        print("\t1. Gerenciamento")
+        print("\t2. Votação")
+        print("\t0. Sair")
+        escolha = input('Digite o número da opção desejada: ')
+        match escolha:
+            case '1':
+                iniciar_management()
+            case '2':
+                iniciar_votacao()
+            case '0':
+                print("Saindo...")
+                break
+            case _:
+                print('Opção inválida. Por favor, tente novamente.')
 iniciar_hub()
