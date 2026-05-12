76ffb1110cbaa853608b9ff0af895f5258a51632
czanluchi <caua.lz@puccampinas.edu.br>
2026-05-12 15:22:49 -0300

    Update voting_sys.py

--- .Vote/voting_sys.py
+++ .Vote/voting_sys.py
@@ -1,3 +1,8 @@
+import mysql.connector
+from datetime import datetime
+import random
+import uuid
+
 #informações do usuário
 info_completa = 0
 while info_completa == 0:
@@ -6,3 +11,46 @@ while info_completa == 0:
     chave_de_acesso = int(input("Digite sua chave de acesso: "))
     if titulo_eleitor != "" or cpf != "" or chave_de_acesso != "":
         info_completa += 1
+
+#verificar banco
+cursor.execute("""
+SELECT * FROM eleitores
+WHERE titulo_eleitor = %s
+AND cpf_inicio = %s
+AND chave_acesso = %s
+""", (titulo_eleitor, cpf, chave_de_acesso))
+
+eleitor = cursor.fetchone()
+
+if eleitor is None:
+    print("Dados inválidos!!!")
+    exit()
+if eleitor["ja_votou"] == True:
+    print("esse eleitor jaa realizou a votação!")
+    exit()
+
+#solicitação do numero do candidato
+confirmarnum = 0
+while confirmarnum == 0:
+    numero_candidato = int(input("Digite o numero do seu candidato: "))
+
+#consulta do candidato no banco de dados
+    cursor.execute("""
+    SELECT * FROM Candidatos
+    WHERE numero = %s
+    """, (numero_candidato,))
+
+    Candidato = cursor.fetchone()
+
+    if Candidato:
+        print("Candidato:")
+        print("Nome:", Candidato["nome_candidato"])
+        print("Número:", Candidato["numcandidato"])
+        print("Partido:", Candidato["partidocandidato"])
+
+    #confirmação de voto
+    confirmar = input("Deseja confirmar seu voto (S/N): ")
+    if confirmar.upper() == "N":
+        continue
+    else:
+        confirmarnum += 1
