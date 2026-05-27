a43998fa036c0c3e265c09e1ffd5c1c4fc365d0f
czanluchi <caua.lz@puccampinas.edu.br>
2026-05-27 09:14:15 -0300

    adicionando opções aos menus

--- .Vote/Management.py
+++ .Vote/Management.py
@@ -1,6 +1,8 @@
 from Abertura_votação import abrir_votacao
 from cadastro_candidato import menu_candidatos
 from cadastro_eleitor import menu_eleitores
+from Encerramento_votacao import fechar_votacao
+from resultado_votacao import menu_resultados
 
 
 def iniciar_management():
@@ -11,6 +13,8 @@ def iniciar_management():
         print("1. Gerenciar eleitores")
         print("2. Gerenciar candidatos")
         print("3. Abrir votacao (mesario)")
+        print("4. Fechar votação (mesario)")
+        print("5. Resultados votação (mesario)")
         print("0. Voltar")
 
         escolha = input("Digite o numero da opcao desejada: ").strip()
@@ -22,6 +26,10 @@ def iniciar_management():
                 menu_candidatos()
             case "3":
                 abrir_votacao()
+            case "4":
+                fechar_votacao()
+            case "5":
+                menu_resultados()
             case "0":
                 gerenciamento_aberto = False
             case _:
