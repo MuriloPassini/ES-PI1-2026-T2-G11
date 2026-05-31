36d6ca6670794e7153bf69bb181773942de197f2
MuriloPassini <murilo.pt4@puccampinas.edu.br>
2026-05-30 22:34:53 -0300

    manutenção e criação de menu de votação

---
+++ .Vote/menu_votacao.py
@@ -0,0 +1,35 @@
+from voting_sys import votacao
+from Abertura_votação import abrir_votacao
+from Encerramento_votacao import fechar_votacao
+from resultado_votacao import menu_resultados
+from LOGS_VOTAÇÃO import menu_auditoria
+
+def iniciar_menu_votacao():
+    menu_votacao_aberto = True
+    while menu_votacao_aberto:
+        print("=" * 10, " V O T A Ç Ã O ", "=" * 10)
+        print(f'{"Area de votação acessada!":^35}')
+        print("1. Votar")
+        print("2. Abrir votacao (mesario)")
+        print("3. Fechar votação (mesario)")
+        print("4. Resultados votação (mesario)")
+        print("5. Auditoria")
+        print("0. Voltar")
+
+        escolha = input("Digite o numero da opcao desejada: ").strip()
+
+        match escolha:
+            case "1":
+                votacao()
+            case "2":
+                abrir_votacao()
+            case "3":
+                fechar_votacao()
+            case "4":
+                menu_resultados()
+            case "5":
+                menu_auditoria()
+            case "0":
+                menu_votacao_aberto = False
+            case _:
+                print("Opcao invalida. Por favor, tente novamente.")
