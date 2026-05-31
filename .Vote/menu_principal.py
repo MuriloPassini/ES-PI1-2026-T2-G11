73beaa477b9a9a95dc7cf2288ced13e156de48b1
Orteip-eng <pietro.egf@puccampinas.edu.br>
2026-05-30 22:40:54 -0300

    Conflito entre pushs

--- .Vote/menu_principal.py
+++ .Vote/menu_principal.py
@@ -1,6 +1,6 @@
 from Management import iniciar_management
 from voting_sys import votacao
-from LOGS_VOTAÇÃO import menu_auditoria
+
 
 def iniciar_hub():
     hub_aberto = True
@@ -9,7 +9,7 @@ def iniciar_hub():
         print(f'{"Bem-vindo ao menu principal!":^35}')
         print(f'{"Selecione uma opcao para continuar:":^35}')
         print("1. Gerenciamento")
-        print("2. Votacao")
+        print("2. Votação")
         print("3. Auditoria")
         print("0. Sair")
 
@@ -20,14 +20,11 @@ def iniciar_hub():
                 iniciar_management()
             case "2":
                 votacao()
-            case "3":
-                menu_auditoria()
             case "0":
                 print("Saindo...")
                 hub_aberto = False
             case _:
                 print("Opcao invalida. Por favor, tente novamente.")
-
 
 if __name__ == "__main__":
     iniciar_hub()
