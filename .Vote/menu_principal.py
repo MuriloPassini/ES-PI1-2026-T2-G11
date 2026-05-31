a6ccd23cbb29af01839490cece3db19d5bca24fe
Bernardo Garcia <bernardo.gb@puccampinas.edu.br>
2026-05-30 22:33:58 -0300

    codigo com os logs

--- .Vote/menu_principal.py
+++ .Vote/menu_principal.py
@@ -1,5 +1,6 @@
 from Management import iniciar_management
 from voting_sys import votacao
+from LOGS_VOTAÇÃO import menu_auditoria
 
 def iniciar_hub():
     hub_aberto = True
@@ -8,7 +9,8 @@ def iniciar_hub():
         print(f'{"Bem-vindo ao menu principal!":^35}')
         print(f'{"Selecione uma opcao para continuar:":^35}')
         print("1. Gerenciamento")
-        print("2. Votação")
+        print("2. Votacao")
+        print("3. Auditoria")
         print("0. Sair")
 
         escolha = input("Digite o numero da opcao desejada: ").strip()
@@ -18,11 +20,14 @@ def iniciar_hub():
                 iniciar_management()
             case "2":
                 votacao()
+            case "3":
+                menu_auditoria()
             case "0":
                 print("Saindo...")
                 hub_aberto = False
             case _:
                 print("Opcao invalida. Por favor, tente novamente.")
+
 
 if __name__ == "__main__":
     iniciar_hub()
